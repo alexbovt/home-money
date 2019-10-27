987f8afc4db5412a7d984ccbea5b5c6a4d786f54
alexbovt <bovttt@gmail.com>
2019-10-27 22:54:45 +0100

    added routing, login and register forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {AuthModule} from './auth/auth.module';
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AuthModule
+    AuthModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
