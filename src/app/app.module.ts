cc9d8aeb1f4158461aa7d88bb741fe68d382af32
alexbovt <bovttt@gmail.com>
2019-10-27 23:39:58 +0100

    added form control and validation for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AuthModule} from './auth/auth.module';
 import {AppRoutingModule} from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -11,10 +12,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AuthModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
